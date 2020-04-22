636ae8c162bf44cf2df66a93db0e757f488c0ec8
yayun.xia <465425854@qq.com>
2020-04-22 19:31:13 +0800

    简化代码

--- app/src/main/java/com/xyy/customview/MainActivity.java
+++ app/src/main/java/com/xyy/customview/MainActivity.java
@@ -1,12 +1,11 @@
 package com.xyy.customview;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatSeekBar;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.SeekBar;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatSeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,14 +15,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         AppCompatSeekBar seekBar = findViewById(R.id.seekbar);
         final RingPercentageView percentageView = findViewById(R.id.view_ring);
-        final TextView tvValue = findViewById(R.id.tv_value);
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 Log.d("seekbar", "progress:" + progress);
                 percentageView.setPercentData(progress);
-                tvValue.setText("Value:" + progress);
             }
 
             @Override
